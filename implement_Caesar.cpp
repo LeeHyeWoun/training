62e8a2facd44c53b3c99088b0f5a2f563963fcd8
LeeHyewoun <silro812@naver.com>
2020-03-28 02:46:57 +0900

    Add implement_Caesar.cpp

---
+++ implement_Caesar.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+날짜 : 2020.03.27.금
+이름 : 이헤원
+
+강의 : 현대암호학응용및실습
+분반 : 02
+
+과제 : 고대 또는 근대 암호 알고리즘 1개 만들기
+선택 : 소스코드 방식 - 시저암호
+*/
+
+void Caesar(char *in, char *en, int num); //평문과 얼만큼 평행이동할 지 입력
+
+int main() {
+	char en[100] = "";	//암호문
+	char plain[100];	//평문
+	int num = 0;		//평행 이동할 정도
+
+	//값 입력받기
+	cout << "\n평문 입력 : ";
+	cin >> plain;
+	cout << "\n평행 이동 : ";
+	cin >> num;
+
+	//출력
+	Caesar(plain, en, num);
+
+	return 0;
+}
+
+void Caesar(char *plain, char *en, int num) {
+	char tmp;
+	int i;
+
+	//입력한 문자 길이만큼 반복
+	for (i=0; *plain; en++, plain++, i++) {
+
+		//문자나 숫자인 경우
+		if (isalnum(*plain) != 0) {
+			//대문자 처리
+			if (isupper(*plain))
+				tmp = 'A';	//65
+			//소문자 처리
+			else if (islower(*plain))
+				tmp = 'a';	//97
+			//숫자 문자일 때
+			else if (isdigit(*plain))
+				tmp = '0';	//48
+
+			//num 값 만큼 값을 변경 (예시 : plain = a, num = 5 --> en = f)
+			*en = (*plain - tmp + num) % (tmp>'0' ? 26:10) + tmp;
+		}
+
+		//문자도 숫자도 아닌 경우 (예시 : !,@,#,$,%,... )
+		else 
+			*en = *plain;
+
+	}
+
+	//포인터 위치 제자리로
+	en -= i;
+
+	//출력
+	cout << "\n암호화 \n > " << en << endl << endl << endl;
+}
